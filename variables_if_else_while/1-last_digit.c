ccdd84a2da473d0c96d597a3c4d5ea15c4021959
Efrenjm <efrenjimenez_96hhotmail.com>
2023-10-19 14:08:54 -0700

    second

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+int last_digit;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+last_digit = n % 10;
+printf("Last digit of %d is %d and is ", n, last_digit);
+if (last_digit > 5)
+{
+	printf("greater than 5");
+}
+else if (last_digit == 0)
+{
+	printf("0");
+}
+else
+{
+	printf("less than 6 and not 0");
+}
+printf("\n");
+return (0);
+}
---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 61; i < 123; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
