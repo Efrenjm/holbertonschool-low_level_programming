9434b5b110a98bf3667100c64470fa570b4302b3
Efrenjm <efrenjimenez_96hhotmail.com>
2023-10-19 21:07:01 -0700

    final test

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,11 +11,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i >= 57; i--)
+	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
-		putchar(44);
-		putchar(' ');
+		if (i!=57)
+		{
+			putchar(44);
+			putchar(32);
+		}
 	}
 	putchar(10);
 	return (0);
