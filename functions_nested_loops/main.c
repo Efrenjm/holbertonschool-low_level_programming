b7fe1456a8de534cfc6d205a70ea51b3f2143660
Efrenjm <efrenjimenez_96hhotmail.com>
2023-10-21 11:59:13 -0700

    lets do this

---
+++ functions_nested_loops/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* more headers goes there */
+/* betty style doc for function main goes there */
+
+/**
+ * print_alphabet - prints the alphabet in order
+ *
+ * Return: Always 0 (Success)
+ */
+int print_alphabet(void)
+{
+        int i;
+
+        for (i = 97; i < 123; i++)
+                putc(i, stdout);
+        putc(10, stdout);
+        return (0);
+}
