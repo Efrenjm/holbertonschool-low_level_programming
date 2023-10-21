4f2eef15a58e1e97d34b09f2e52d1e14b54cfa65
Efrenjm <efrenjimenez_96hhotmail.com>
2023-10-21 14:34:08 -0700

    please work

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - prints add
+ *@a: 1 value
+ *@b: 2 value
+ * main -code
+ * Return: suma
+ */
+int add(int a, int b)
+{
+	int sum;
+	sum = a + b;
+	return (sum);
+}
