fe8d21a1cc14c6a22d88dfecdde3394135f90177
Efrenjm <efrenjimenez_96hhotmail.com>
2023-10-26 20:20:20 -0700

    add betty style

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_alphabet - prints alphabet
- * main - funcion principal
+ * reset_to_98 - set the value of n to 98
+ * @n: pointer to the number to restore
  * Return: 0
  */
 void reset_to_98(int *n)
