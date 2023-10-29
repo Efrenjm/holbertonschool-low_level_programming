b89f5e8646cf774228a2096f5f3637bc21e90886
Efrenjm <efrenjimenez_96hhotmail.com>
2023-10-28 17:05:42 -0700

    add correctly the new files

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - swap the values of two integers
+ * @s: string that will be counted 
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
