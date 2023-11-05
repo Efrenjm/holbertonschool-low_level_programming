95c8d71083d558ef2a70a836a002a7e9667e456d
Efrenjm <efrenjimenez_96@hotmail.com>
2023-11-05 08:39:18 -0800

    check thi

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,16 +1,30 @@
 #include "main.h"
 
 /**
- * _puts_recursion - writes a string with recursion
- * @s: string
- * Return: always 0.
+ * find_sqrt - find the natural square root
+ * @n: number
+ * @guess: the natural number that could be the root
+ * Return: natural root or -1.
  */
-void _puts_recursion(char *s)
+int find_sqrt(int n, int guess)
 {
-	if (*s == '\0') {
-        	_putchar('\n');
-        	return;
-    	}
-    _putchar(*s);
-    _puts_recursion(s + 1);
+	if (guess * guess == n)
+		return (guess);
+	if (guess * guess > n)
+		return (-1);
+	return (find_sqrt(n, guess + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number
+ * Return: natural root or -1.
+ */
+
+int _sqrt_recursion(int n)
+{
+        if (n < 0)
+                return (-1);
+        if (n == 0 || n == 1)
+                return (n);
+        return (find_sqrt(n, 1));
 }
