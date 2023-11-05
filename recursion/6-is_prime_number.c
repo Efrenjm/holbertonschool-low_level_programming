bf5df250f425851f497f00e95b643e80d755fe09
Efrenjm <efrenjimenez_96@hotmail.com>
2023-11-05 08:52:56 -0800

    add betty

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -4,28 +4,28 @@
  * is_prime_recursive - recursive function to check if the number is prime
  * @n: number to be tested
  * @divisor: divisor to test
- * Return: always 0.
+ * Return: 1 if it is prime or 0 if it is not.
  */
 
 int is_prime_recursive(int n, int divisor)
 {
 	if (n <= 1)
-		return 0;
+		return (0);
 	if (divisor == 1)
-		return 1;
+		return (1);
 	if (n % divisor == 0)
-		return 0;
-	return is_prime_recursive(n, divisor - 1);
+		return (0);
+	return (is_prime_recursive(n, divisor - 1));
 }
 
 /**
  * is_prime_number - recursive function to check if the number is prime
  * @n: number to be tested
- * Return: always 0.
+ * Return: 1 if it is prime or 0 if it is not.
  */
 int is_prime_number(int n)
 {
 	if (n <= 1)
-		return 0;
-	return is_prime_recursive(n, n - 1);
+		return (0);
+	return (is_prime_recursive(n, n - 1));
 }
