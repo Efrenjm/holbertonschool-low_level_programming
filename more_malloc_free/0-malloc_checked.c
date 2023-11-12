748b37b7baaa92bc16e8de124ee2a8dea3f552e1
Efrenjm <efrenjimenez_96@hotmail.com>
2023-11-11 18:39:51 -0800

    new try

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - allocate memory
+ * @b: bytes to allocate
+ * Return: Pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *allocated_memory = malloc(b);
+	if (allocated_memory == NULL)
+		exit(98);
+
+	return (allocated_memory);
+}
