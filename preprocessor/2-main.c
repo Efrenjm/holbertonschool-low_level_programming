11ab334ebcaf222402dbea47cae0345d52f5402a
Efrenjm <efrenjimenez_96@hotmail.com>
2023-11-12 10:11:15 -0800

    add betty style

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main() {
-    printf("%s\n", __FILE__);
-    return 0;
+/**
+ * main - prints the name of the c file
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
 }
