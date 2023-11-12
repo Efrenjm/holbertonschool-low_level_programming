05b660cd70d012c200be292cbf8af5ae8f4be06c
Efrenjm <efrenjimenez_96@hotmail.com>
2023-11-11 18:57:57 -0800

    new ex

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * string_nconcat - partially concatenate two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: bytes from second string
+ * Return: s1 + first n bytes of s2
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	size_t s1_len, s2_len;
+	char *concat;
+	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
+
+	if (n >= s2_len)
+		n = s2_len;
+
+	concat = malloc(s1_len + n + 1);
+
+	if (concat != NULL) {
+		strcpy(concat, s1);
+		strncat(concat, s2, n);
+	}
+
+	return (concat);
+}
---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * _calloc - mimics calloc function
+ * @nmemb: number of elements
+ * @size: bytes per element
+ * Return: pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *alloc;
+	if (nmemb == 0 || size == 0)
+		return NULL;
+
+	alloc = malloc(nmemb * size);
+
+	if (alloc != NULL)
+		memset(alloc, 0, nmemb * size);
+
+	return alloc;
+}
