2ce1efab62a2e11af3fb413b3dcdb0e64eb716ad
Efrenjm <efrenjimenez_96@hotmail.com>
2023-11-18 18:22:23 -0800

    Structures and typedef, excercises 1, 2 and 3

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * init_dog - dog intializer.
+ * @d: dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: Always 0.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = strdup(name);
+	d->owner = strdup(owner);
+	d->age = age;
+}
