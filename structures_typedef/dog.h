ddfa8d23d7944b6913c74d6c3ed00c1d00b0dee6
Efrenjm <efrenjimenez_96@hotmail.com>
2023-11-18 18:30:21 -0800

    add coding style

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,11 @@
-#include <stdlib.h>
-
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure of dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 struct dog
 {
 	char *name;
@@ -9,3 +15,5 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+#endif
