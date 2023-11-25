8e455654b250fb7c12b39684cb3f562556f883df
Efrenjm <efrenjimenez_96@hotmail.com>
2023-11-25 14:14:49 -0800

    second excercise

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - print string
+ * @separator: separator
+ * @n: number of arguments
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+	char *arg;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		arg = va_arg(args, char *);
+		if (arg == NULL)
+			printf("(nil)");
+		else
+			printf("%s", arg);
+
+		if (separator != NULL || i < n - 1)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(args);
+}
