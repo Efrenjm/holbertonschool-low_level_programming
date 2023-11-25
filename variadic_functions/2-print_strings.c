e7a989b1c9d9826dad433a60f0bca02e68edfa42
Efrenjm <efrenjimenez_96@hotmail.com>
2023-11-25 14:17:10 -0800

    new try

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s", arg);
 
-		if (separator != NULL || i < n - 1)
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
 
