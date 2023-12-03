ebd1da0d95151e142bf7a2a3c6f18ed730021735
Efrenjm <efrenjimenez_96@hotmail.com>
2023-12-03 10:29:37 -0800

    new test

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * clear_bit - set the value to 1 of a bit index
+ * @n: address to the int number
+ * @index: index of the bit
+ * Return: 1 if worked, 0 if not
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	*n &= ~(1UL << index);
+	return (1);
+}
