bfaf9c3587600384e72a8f69b4a7c8f90f523a4e
Efrenjm <efrenjimenez_96@hotmail.com>
2023-12-02 19:42:06 -0800

    new test

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -21,25 +21,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	current = *h;
 	if (idx == 0)
-		new_node = add_dnodeint(h, n);
-	for (count = 0; count < idx; count++)
+		return (add_dnodeint(h, n));
+	for (count = 0; count <= idx; count++)
 	{
-		if (current == NULL)
-		{
-			free(new_node);
-			return (NULL);
-		}
-		current = current->next;
+		if (current->next == NULL)
+			return (add_dnodeint_end(h, n));
+		if (count < idx)
+			current = current->next;
 	}
-	if (current == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->next = current->next;
-	new_node->prev = current;
-	if (current->next != NULL)
-		current->next->prev = new_node;
-	current->next = new_node;
+	new_node->next = current;
+	new_node->prev = current->prev;
+	current->prev->next = new_node;
+	current->prev = new_node;
 	return (new_node);
 }
