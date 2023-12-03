6958bcc2104d00d9aaef8347e0156fcdf4a511ae
Efrenjm <efrenjimenez_96@hotmail.com>
2023-12-02 20:13:15 -0800

    new test

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node;
 	dlistint_t *current;
 	unsigned int count;
-	
+
 	if (h == NULL)
 		return (NULL);
 	if (idx == 0)
@@ -28,14 +28,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (current == NULL)
 		return (NULL);
-	
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = current->next;
 	new_node->prev = current;
-	
+
 	if (current->next != NULL)
 		current->next->prev = new_node;
 	current->next = new_node;
