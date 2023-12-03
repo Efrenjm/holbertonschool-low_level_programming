99b6b38c110cd57496c90ca94308387d9f3ca718
Efrenjm <efrenjimenez_96@hotmail.com>
2023-12-02 20:12:11 -0800

    new test

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,26 +14,30 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node;
 	dlistint_t *current;
 	unsigned int count;
-
+	
+	if (h == NULL)
+		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	current = *h;
+	for (count = 0; count < idx - 1; count++)
+	{
+		if (current == NULL)
+			return (NULL);
+		current = current->next;
+	}
+	if (current == NULL)
+		return (NULL);
+	
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	current = *h;
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-	for (count = 0; count <= idx; count++)
-	{
-		if (current->next == NULL && count == idx)
-			return (add_dnodeint_end(h, n));
-		else if (current->next == NULL && count < idx)
-			return (NULL);
-		if (count < idx)
-			current = current->next;
-	}
-	new_node->next = current;
-	new_node->prev = current->prev;
-	current->prev->next = new_node;
-	current->prev = new_node;
+	new_node->next = current->next;
+	new_node->prev = current;
+	
+	if (current->next != NULL)
+		current->next->prev = new_node;
+	current->next = new_node;
 	return (new_node);
 }
