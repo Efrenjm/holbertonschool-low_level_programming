eee193a607d104e4c1aa1200509c58a33ee71e53
Efrenjm <efrenjimenez_96@hotmail.com>
2023-12-03 10:21:58 -0800

    add betty style

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * set_bit - set the value to 1 of a bit index
  * @n: address to the int number
  * @index: index of the bit
@@ -8,8 +8,8 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
-		return -1;
+		return (-1);
 
 	*n |= (1UL << index);
-	return 1;
+	return (1);
 }
