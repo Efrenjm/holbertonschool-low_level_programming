b15f938452b447fb9c97224699e7b06e1e99ea9a
Efrenjm <efrenjimenez_96@hotmail.com>
2023-12-03 10:58:44 -0800

    first extra excercise

---
+++ bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * get_endianness - get endianess of the system
+ * Return: 0 if little endian, 1 if big endian
+ */
+int get_endianness(void) {
+	unsigned int num = 1;
+	char *byte_ptr = (char *)&num;
+
+	return (*byte_ptr == 1) ? 1 : 0;
+}
