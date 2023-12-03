f5f345ee530fe76bcfda5a42e65a10a8f18617a5
Efrenjm <efrenjimenez_96@hotmail.com>
2023-12-02 19:12:48 -0800

    test of 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -3,22 +3,43 @@
 #include <string.h>
 #include "lists.h"
 /**
- * free_dlistint - frees a list_t
- * @head: head of the list
+ * insert_dnodeint_at_index - insert node at index
+ * @h: head of the list
+ * @idx: index where to insert
+ * @n: value to insert
  * Return: none
  */
-void free_dlistint(dlistint_t *head)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp;
+	dlistint_t *new_node;
+	dlistint_t *current;
+	unsigned int count;
 
-	if (head != NULL)
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	current = *h;
+	if (idx == 0)
+		new_node = add_dnodeint(h, n);
+	for (count = 0; count < idx; count++)
 	{
-		while (head->next != NULL)
+		if (current == NULL)
 		{
-			temp = head;
-			head = head->next;
-			free(temp);
+			free(new_node);
+			return (NULL);
 		}
-		free(head);
+		current = current->next;
 	}
+	if (current == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = current->next;
+	new_node->prev = current;
+	if (current->next != NULL)
+		current->next->prev = new_node;
+	current->next = new_node;
+	return (new_node);
 }
