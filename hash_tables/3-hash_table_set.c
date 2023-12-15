7f4162830ecb4467a45b1652311322ece08e27dc
Efrenjm <efrenjimenez_96@hotmail.com>
2023-12-14 17:54:25 -0800

    set new value

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+/**
+ * hash_table_set - set new element to hash table
+ * @ht: direction of the hash table
+ * @key: key to find
+ * @value: value to set
+ * Return: 1 if success else 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node, *current;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			if (current->value == NULL)
+				return (0);
+			return (1);
+		}
+		current = current->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
