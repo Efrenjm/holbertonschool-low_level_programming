d7737794612b97d26d9b35c2dc23efdd8e3288e4
Efrenjm <efrenjimenez_96@hotmail.com>
2023-12-14 18:13:43 -0800

    get value

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_get - get value from key
+ * @ht: hash table address
+ * @key: key to get
+ * Return: value
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (current->value);
+		current = current->next;
+	}
+
+	return (NULL);
+}
